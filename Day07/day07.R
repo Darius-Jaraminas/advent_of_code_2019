c80b98d53d7e4552e001ac731a5d5df060a7bafa
Darius Jaraminas <darius.jaraminas@euromonitor.com>
2019-12-10 09:11:07 +0200

    Day 7 phase 2 solution. Cleaned up code

--- Day07/day07.R
+++ Day07/day07.R
@@ -2,76 +2,29 @@ library(dplyr)
 
 source("fun.R")
 
-t1 <- file("test1.txt") %>%
-  readLines() %>% 
-  strsplit(",") %>% 
-  `[[`(1) %>% 
-  as.numeric()
+# phase 1
+t1 <- read_program("test1.txt")
 r1 <- run_one_phase(x = t1, phase = c(4, 3, 2, 1, 0))
 r1 == 43210
 
-t2 <- file("test2.txt") %>%
-  readLines() %>% 
-  strsplit(",") %>% 
-  `[[`(1) %>% 
-  as.numeric()
+t2 <- read_program("test2.txt")
 r2 <- run_one_phase(x = t2, phase = c(0, 1, 2, 3, 4))
 r2 == 54321
 
-t3 <- file("test3.txt") %>%
-  readLines() %>% 
-  strsplit(",") %>% 
-  `[[`(1) %>% 
-  as.numeric()
+t3 <- read_program("test3.txt")
 r3 <- run_one_phase(x = t3, phase = c(1, 0, 4, 3, 2))
 r3 == 65210
 
-
-inp <- file("input.txt") %>%
-  readLines() %>% 
-  strsplit(",") %>% 
-  `[[`(1) %>% 
-  as.numeric()
-all_combs <- expand.grid(0:4, 0:4, 0:4, 0:4, 0:4)
-w <- apply(all_combs, 1, function(x){all(0:4 %in% x)})
-all_combs <- all_combs[w, ]
-thr <- numeric(length(all_combs))
-for (i in 1:nrow(all_combs)){
-  phase_i <- as.numeric(all_combs[i, ])
-  thr[i] <- run_one_phase(x = inp, phase = phase_i)
-}
-r <- max(thr)
+inp <- read_program("input.txt")
+rp1 <- find_max_signal(inp, ph = 0:4, fun = run_one_phase)
 
 # phase 2
+t4 <- read_program("test4.txt")
+r4 <- run_feedback_loop_one_phase(x = t4, phase = c(9, 8, 7, 6, 5))
+r4 == 139629729
 
-t4 <- file("test4.txt") %>%
-  readLines() %>% 
-  strsplit(",") %>% 
-  `[[`(1) %>% 
-  as.numeric()
+t5 <- read_program("test5.txt")
+r5 <- run_feedback_loop_one_phase(x = t5, phase = c(9, 7, 8, 5, 6))
+r5 == 18216
 
-
-o1 <- run_program3(x = t4, input = c(9, 0))
-o2 <- run_program3(x = t4, input = c(8, unique(o1$out)))
-o3 <- run_program3(x = t4, input = c(7, unique(o2$out)))
-o4 <- run_program3(x = t4, input = c(6, unique(o3$out)))
-o5 <- run_program3(x = t4, input = c(5, unique(o4$out)))
-
-for (i in 1:5){
-  o1 <- run_program3(x = t4, input = c(9, unique(o5$out)))
-  o2 <- run_program3(x = t4, input = c(8, unique(o1$out)))
-  o3 <- run_program3(x = t4, input = c(7, unique(o2$out)))
-  o4 <- run_program3(x = t4, input = c(6, unique(o3$out)))
-  o5 <- run_program3(x = t4, input = c(5, unique(o4$out)))
-  print(o5)
-}
-
-
-
-https://www.reddit.com/r/adventofcode/comments/e7aqcb/2019_day_7_part_2_confused_with_the_question/
-
-r3 <- run_one_phase(x = t3, phase = c(9, 8, 7, 6, 5))
-r3 == 65210
-
-
-
+rp2 <- find_max_signal(inp, ph = 5:9, fun = run_feedback_loop_one_phase)
--- Day07/fun.R
+++ Day07/fun.R
@@ -27,8 +27,6 @@ run_program2 <- function(x, input){
     }
     if (opcode == 4){
       out <- c(out, x[pos1])
-      # input <- c(input, out)
-      # print(x[pos1])
       at <- at + 2
     }
     if (opcode == 5){
@@ -62,8 +60,7 @@ run_program2 <- function(x, input){
   return(out)
 }
 
-run_program3 <- function(x, input){
-  at <- 1
+run_program3 <- function(x, at, input){
   halt <- FALSE
   input_counter <- 1
   out <- numeric()
@@ -95,9 +92,8 @@ run_program3 <- function(x, input){
     }
     if (opcode == 4){
       out <- c(out, x[pos1])
-      # input <- c(input, out)
-      # print(x[pos1])
       at <- at + 2
+      return(list(x = x, at = at, out = out, halt = FALSE))
     }
     if (opcode == 5){
       if (x[pos1] != 0){
@@ -127,7 +123,7 @@ run_program3 <- function(x, input){
       halt <- TRUE
     }
   }
-  return(out)
+  return(list(x = x, at = at, out = out, halt = TRUE))
 }
 
 run_one_phase <- function(x, phase){
@@ -140,5 +136,44 @@ run_one_phase <- function(x, phase){
 }
 
 run_feedback_loop_one_phase <- function(x, phase){
-  
+  aa <- run_program3(x = x, at = 1, input = c(phase[1], 0))
+  bb <- run_program3(x = x, at = 1, input = c(phase[2], aa$out))
+  cc <- run_program3(x = x, at = 1, input = c(phase[3], bb$out))
+  dd <- run_program3(x = x, at = 1, input = c(phase[4], cc$out))
+  ee <- run_program3(x = x, at = 1, input = c(phase[5], dd$out))
+  while (!ee$halt){
+    aa <- run_program3(x = aa$x, at = aa$at, input = ee$out)
+    bb <- run_program3(x = bb$x, at = bb$at, input = aa$out)
+    cc <- run_program3(x = cc$x, at = cc$at, input = bb$out)
+    dd <- run_program3(x = dd$x, at = dd$at, input = cc$out)
+    ee <- run_program3(x = ee$x, at = ee$at, input = dd$out)
+    if (!ee$halt){
+      last_ee <- ee$out
+    }
+  }
+  return(last_ee)
+}
+
+find_max_signal <- function(inp, ph, fun){
+  all_combs <- expand.grid(ph, ph, ph, ph, ph)
+  w <- apply(all_combs, 1, function(x, y){all(y %in% x)}, y = ph)
+  all_combs <- all_combs[w, ]
+  ncomb <- nrow(all_combs)
+  thr <- numeric(ncomb)
+  for (i in 1:ncomb){
+    phase_i <- as.numeric(all_combs[i, ])
+    thr[i] <- fun(x = inp, phase = phase_i)
+  }
+  return(max(thr))
+}
+
+read_program <- function(fnm){
+  con <- file(fnm)
+  out <- con %>%
+    readLines() %>% 
+    strsplit(",") %>% 
+    `[[`(1) %>% 
+    as.numeric()
+  close(con)
+  return(out)
 }
